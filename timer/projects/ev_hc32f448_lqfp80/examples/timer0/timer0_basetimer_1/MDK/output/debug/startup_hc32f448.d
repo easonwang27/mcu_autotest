cd869337f0db30c5cee6e5bdd5303f91f98faec5
Eason Wang <easonwang27@163.com>
2024-04-16 23:47:43 +0800

    remove useless file

--- timer/projects/ev_hc32f448_lqfp80/examples/timer0/timer0_basetimer_1/MDK/output/debug/startup_hc32f448.d
+++
@@ -1,1 +0,0 @@
-.\output\debug\startup_hc32f448.o: startup_hc32f448.s
--- timer/projects/ev_hc32f448_lqfp80/examples/timer0/timer0_basetimer_1/MDK/output/debug/tca9539.d
+++
@@ -1,5 +0,0 @@
-.\output\debug\tca9539.o: ../../../../../../drivers/bsp/components/tca9539/tca9539.c
-.\output\debug\tca9539.o: ../../../../../../drivers/bsp/components/tca9539/tca9539.h
-.\output\debug\tca9539.o: ../../../../../../drivers/hc32_ll_driver/inc/hc32_ll_def.h
-.\output\debug\tca9539.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\output\debug\tca9539.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
